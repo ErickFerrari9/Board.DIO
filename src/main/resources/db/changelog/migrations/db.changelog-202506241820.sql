b3988da301784f4a967aae7780197737c22b6180
Erick <ferrari_erick@hotmail.com>
2025-06-25 13:08:25 -0400

    migrations

--- src/main/resources/db/changelog/migrations/db.changelog-202506241820.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202506241820.sql
@@ -1,8 +1,10 @@
 -- liquibase formatted sql
 
--- changeset erick:240620251800
-CREATE TABLE BOARDS (
-                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                           name VARCHAR(150)
-);
+-- changeset erick:202506241820
+--comment: boards table create
+CREATE TABLE BOARDS
+(
+    id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150)
+)ENGINE=InnoDB;
 --rollback DROP TABLE BOARDS
